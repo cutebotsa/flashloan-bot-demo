5555a715d087abd396bc82c6a05412712cee592b
Your Name <you@example.com>
2026-01-03 15:07:35 -0500

    Remove node_modules and package-lock.json from tracking and update .gitignore

--- node_modules/@uniswap/smart-order-router/build/module/providers/v4/uri-subgraph-provider.d.ts
+++
@@ -1,4 +0,0 @@
-import { URISubgraphProvider } from '../uri-subgraph-provider';
-import { IV4SubgraphProvider, V4SubgraphPool } from './subgraph-provider';
-export declare class V4URISubgraphProvider extends URISubgraphProvider<V4SubgraphPool> implements IV4SubgraphProvider {
-}
